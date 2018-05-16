e6f99219fcb3019e4b51776a8d1ec5ad599c3d47
tabcompany <info.tabcompany@gmail.com>
2018-05-16 19:18:04 +0430

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private Stat health, energy, shield;
+
+	// Use this for initialization
+	void Awake () {
+		health.Initialize ();
+		energy.Initialize ();
+		shield.Initialize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// For Health
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			health.CurrentVal -= 10;
+		}
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			health.CurrentVal += 10;
+		}
+
+		// For Energy
+		if (Input.GetKeyDown(KeyCode.A)) {
+			energy.CurrentVal -= 10;
+		}
+
+		if (Input.GetKeyDown(KeyCode.S)) {
+			energy.CurrentVal += 10;
+		}
+
+		// For Shield
+		if (Input.GetKeyDown(KeyCode.Z)) {
+			shield.CurrentVal -= 10;
+		}
+
+		if (Input.GetKeyDown(KeyCode.X)) {
+			shield.CurrentVal += 10;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Unity 5 Health Bar
+
+![Unity 5 Health Bar Intro Image](https://github.com/tabcompany/Project-s-imags/blob/master/Unity_HealthBar.png?raw=true)
+
+
+## This project will teach you how to create a dynamic health bar in Unity5. The bar can be used to visualize anything from health to experience. which will be demonstrated through out the series.
+
+To create the health bar we will be using the the Unity UI system in combination with C# scripting.
